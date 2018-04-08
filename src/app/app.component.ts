5ec71b5f3bdaded04f0eb5b1f1ed173ce79f7028
VictorCoding <victor.ramos1@gmail.com>
2018-04-08 17:39:49 -0500

    loading tweets from api and displaying. barebones set up for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:  `
+    <header>
+      <h3>Language Patterns</h3>
+    </header>
+    <div class="body-container">
+      <input placeholder="Search tweets">
+      <div *ngFor="let tweet of tweets">
+        location: <strong>{{ tweet.user.location }}</strong> {{ tweet.text }}
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  tweets = []
+  constructor(
+    private http: HttpClient
+  ) {}
+  ngOnInit() {
+    this.http
+      .get('https://language-patterns-omcjimutwm.now.sh')
+      .subscribe(res => {
+        this.tweets = res['statuses'];
+      });
+  }
 }
